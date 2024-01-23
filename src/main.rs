daadd7a65d90f432995b7b81ca5c68c5c3d1270c
ryardley <contact@rudiyardley.com>
2024-01-23 14:38:59 +1100

    Update

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,35 @@ use std::str::FromStr;
 use anyhow::*;
 use bip32::{DerivationPath, Seed, XPrv};
 use bip39::{Language, Mnemonic};
+use hex::{decode, encode};
 use libsecp256k1::{PublicKey, SecretKey};
 use rand::rngs::OsRng;
 use tiny_keccak::{Hasher, Keccak};
 
-fn eth_from_secret() -> Result<()> {
-    println!("Generating an Ethereum address");
-
-    // Hard hat secret key and public address
-    let hh_secret = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-    let hh_address = "f39Fd6e51aad88F6F4ce6aB8827279cffFb92266".to_lowercase();
-
-    // Use the secp256k1 lib to generate a public key
-    let key_bytes = &hex::decode(hh_secret).unwrap();
-    let secret_key = SecretKey::parse_slice(key_bytes)?;
-    let public_key = PublicKey::from_secret_key(&secret_key);
-    println!("Public Key: {:?}", hex::encode(public_key.serialize()));
-
-    // Get the keccak hash of the address
+fn keccak_hash(input: [u8; 65]) -> [u8; 32] {
     let mut keccak = Keccak::v256();
     let mut address_slice = [0u8; 32];
-    keccak.update(&public_key.serialize()[1..]);
+    keccak.update(&input[1..]);
     keccak.finalize(&mut address_slice);
+    address_slice
+}
 
-    // Take the last 20 bytes from the address
-    let address = hex::encode(&address_slice[12..]);
-    println!("Address: {:?}", address);
+fn eth_from_secret() -> Result<()> {
+    let secret_hex = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    let secret_bytes = &decode(secret_hex)?;
+    let secret_key = SecretKey::parse_slice(secret_bytes)?;
+    let public_key = PublicKey::from_secret_key(&secret_key);
+    let public_bytes = public_key.serialize();
 
-    // Addresses match
-    assert_eq!(hh_address, address);
+    println!("Public Key: {:?}", encode(public_bytes));
+
+    let public_hash = keccak_hash(public_bytes);
+    let last_20_bytes = &public_hash[12..];
+    let address_hex = encode(last_20_bytes);
+
+    println!("Address: {}", address_hex);
+
+    assert_eq!("f39fd6e51aad88f6f4ce6ab8827279cfffb92266", address_hex);
 
     Ok(())
 }
